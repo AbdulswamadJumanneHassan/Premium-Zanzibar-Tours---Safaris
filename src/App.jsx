7272de6dc3222592fea755235d758a85d3f3998f
iammladejr <abdulswamadjumanne6@gmail.com>
2026-06-21 23:39:50 +0300

    feat: add floating WhatsApp contact button to global layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar.jsx';
 import Footer from './components/layout/Footer.jsx';
 import ScrollToTop from './components/layout/ScrollToTop.jsx';
 import ScrollUpButton from './components/layout/ScrollUpButton.jsx';
+import WhatsAppButton from './components/layout/WhatsAppButton.jsx';
 import Home from './pages/Home.jsx';
 import Contact from './pages/Contact.jsx';
 
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <ScrollToTop />
       <ScrollUpButton />
+      <WhatsAppButton />
       <div className="min-h-screen bg-white text-charcoal">
         <Navbar />
         
